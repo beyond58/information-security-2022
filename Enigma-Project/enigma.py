325626e5a77dc33d603d470c4990cdcc02b00a75
gomoji <beyond58@nate.com>
2022-09-20 00:22:05 +0900

    feat : enigma rotate func

---
+++ Enigma-Project/enigma.py
@@ -0,0 +1,143 @@
+# Enigma Template Code for CNU Information Security 2022
+# Resources from https://www.cryptomuseum.com/crypto/enigma
+
+# This Enigma code implements Enigma I, which is utilized by
+# Wehrmacht and Luftwaffe, Nazi Germany.
+# This version of Enigma does not contain wheel settings, skipped for
+# adjusting difficulty of the assignment.
+
+from copy import deepcopy
+from ctypes import ArgumentError
+
+# Enigma Components
+ETW = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+WHEELS = {
+    "I": {
+        "wire": "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        "turn": 16
+    },
+    "II": {
+        "wire": "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        "turn": 4
+    },
+    "III": {
+        "wire": "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        "turn": 21
+    }
+}
+
+UKW = {
+    "A": "EJMZALYXVBWFCRQUONTSPIKHGD",
+    "B": "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+    "C": "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+}
+
+# Enigma Settings
+SETTINGS = {
+    "UKW": None,
+    "WHEELS": [],
+    "WHEEL_POS": [],
+    "ETW": ETW,
+    "PLUGBOARD": []
+}
+
+
+def apply_settings(ukw, wheel, wheel_pos, plugboard):
+    if not ukw in UKW:
+        raise ArgumentError(f"UKW {ukw} does not exist!")
+    SETTINGS["UKW"] = UKW[ukw]
+
+    wheels = wheel.split(' ')
+    for wh in wheels:
+        if not wh in WHEELS:
+            raise ArgumentError(f"WHEEL {wh} does not exist!")
+        SETTINGS["WHEELS"].append(WHEELS[wh])
+
+    wheel_poses = wheel_pos.split(' ')
+    for wp in wheel_poses:
+        if not wp in ETW:
+            raise ArgumentError(f"WHEEL position must be in A-Z!")
+        SETTINGS["WHEEL_POS"].append(ord(wp) - ord('A'))
+
+    plugboard_setup = plugboard.split(' ')
+    for ps in plugboard_setup:
+        if not len(ps) == 2 or not ps.isupper():
+            raise ArgumentError(f"Each plugboard setting must be sized in 2 and caplitalized; {ps} is invalid")
+        SETTINGS["PLUGBOARD"].append(ps)
+
+
+# Enigma Logics Start
+
+# Plugboard
+def pass_plugboard(input):
+    for plug in SETTINGS["PLUGBOARD"]:
+        if str.startswith(plug, input):
+            return plug[1]
+        elif str.endswith(plug, input):
+            return plug[0]
+
+    return input
+
+
+# ETW
+def pass_etw(input):
+    return SETTINGS["ETW"][ord(input) - ord('A')]
+
+
+# Wheels
+def pass_wheels(input, reverse=False):
+    # Implement Wheel Logics
+    # Keep in mind that reflected signals pass wheels in reverse order
+    wheel = SETTINGS["WHEELS"]
+    wheel_pos = SETTINGS["WHEEL_POS"]
+    if reverse:
+        for i in range(len(wheel)-1,-1,-1):
+            print(wheel[i])
+            input = ETW[(wheel[i]["wire"].find(ETW[(ETW.find(input)+wheel_pos[i])%26])-wheel_pos[i]+26)%26]
+    else:
+        for i in range(len(wheel)):
+            input = ETW[(ETW.find(wheel[i]["wire"][(ETW.find(input)+wheel_pos[i])%26])-wheel_pos[i]+26)%26]
+    return input
+
+
+# UKW
+def pass_ukw(input):
+    return SETTINGS["UKW"][ord(input) - ord('A')]
+
+
+# Wheel Rotation
+def rotate_wheels():
+    # Implement Wheel Rotation Logics
+    wheel = SETTINGS["WHEELS"]
+    wheel_pos =  SETTINGS["WHEEL_POS"]
+    if wheel_pos[0] == wheel[0]["turn"]:
+        if wheel_pos[1] == wheel[1]["turn"]:
+            wheel_pos[2] += 1
+        wheel_pos[1] += 1
+    wheel_pos[0] += 1
+    pass
+
+
+# Enigma Exec Start
+plaintext = input("Plaintext to Encode: ")
+ukw_select =input("Set Reflector (A, B, C): ")
+wheel_select =input("Set Wheel Sequence L->R (I, II, III): ")
+wheel_pos_select =input("Set Wheel Position L->R (A~Z): ")
+plugboard_setup = input("Plugboard Setup: ")
+
+apply_settings(ukw_select, wheel_select, wheel_pos_select, plugboard_setup)
+
+for ch in plaintext:
+    rotate_wheels()
+
+    encoded_ch = ch
+
+    encoded_ch = pass_plugboard(encoded_ch)
+    encoded_ch = pass_etw(encoded_ch)
+    encoded_ch = pass_wheels(encoded_ch)
+    encoded_ch = pass_ukw(encoded_ch)
+    encoded_ch = pass_wheels(encoded_ch, reverse = True)
+    encoded_ch = pass_plugboard(encoded_ch)
+
+    print(encoded_ch, end='')
